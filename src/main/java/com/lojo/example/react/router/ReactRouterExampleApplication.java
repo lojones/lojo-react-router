6dae9002684ec02414c42fc9786b44d591ef6d56
Login Jones <login.jones@gmail.com>
2017-08-13 16:41:38 -0400

    genesis

---
+++ src/main/java/com/lojo/example/react/router/ReactRouterExampleApplication.java
@@ -0,0 +1,12 @@
+package com.lojo.example.react.router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactRouterExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactRouterExampleApplication.class, args);
+	}
+}
